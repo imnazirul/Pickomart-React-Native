fba140437c8c595e4c163398994cbdfbb615d8f0
Nazirul <nazirul.me01c@gmail.com>
2025-02-17 23:44:59 +0600

    add back navigation support in WebView; configure EAS build settings and project ID

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { WebView } from 'react-native-webview';
+import { BackHandler } from 'react-native';
 
 const index = () => {
+  const webViewRef = useRef<WebView | null>(null);
+  const [canGoBack, setCanGoBack] = useState(true);
+
+  useEffect(() => {
+    const backAction = () => {
+      if (canGoBack && webViewRef.current) {
+        webViewRef.current.goBack();
+        return true; // Prevents app from closing
+      }
+      return false; // Allows app to close if no back history
+    };
+
+    BackHandler.addEventListener('hardwareBackPress', backAction);
+
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', backAction);
+    };
+  }, [canGoBack]);
+  
   return (
     <SafeAreaView style={{flex:1, backgroundColor:"#161622"}}>
-      <WebView
+      <WebView ref={webViewRef}
       style={styles.container}
       source={{ uri: 'https://pickomart.pythonanywhere.com/' }}
     />
